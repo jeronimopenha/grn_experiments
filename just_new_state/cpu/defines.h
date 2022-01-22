449ad4090e7de88c0f1c05fc7942313c232bf0c5
Jeronimo Costa Penha <jeronimo@g42.casa>
2022-01-22 17:20:04 -0300

    Starting tests with some ideias to improve the grn codes.

---
+++ just_new_state/cpu/defines.h
@@ -0,0 +1,40 @@
+/*
+ * Defines.h.
+ * This file was created to give an index number for each gene node.
+ * In the code, the index is important and not the name, but for debugging
+ * the name is necessary.
+ */
+#define Squaline 0
+#define Lanosterol 1
+#define Acetyl_CoA 2
+#define HMG_CoA_Reductase_RNA 3
+#define Cyp51_RNA 4
+#define Septdehydrocholesterol 5
+#define Insig_SREBP_SCAP 6
+#define FPP_Synthase_gene 7
+#define HMG_CoA_Reductase_gene 8
+#define Acetyl_CoA_acetyltransferase_RNA 9
+#define Statins 10
+#define Acetyl_CoA_acetyltransferase_gene 11
+#define Desmosterol 12
+#define Mevalonic_Acid 13
+#define Acetoacetyl_CoA 14
+#define Mevalonyl_pyrophosphate 15
+#define HMG_CoA 16
+#define Dimethyl_allyl_pyrophosphate 17
+#define HMG_CoA_Reductase 18
+#define FPP_Synthase 19
+#define HMG_CoA_Synthase_RNA 20
+#define SREBP_SCAP 21
+#define Geranyl_pyrophosphate 22
+#define Cyp51 23
+#define Isopentenyl_pyrophosphate 24
+#define Acetyl_CoA_acetyltransferase 25
+#define Farnesyl_pyrophosphate 26
+#define mSREBP 27
+#define FPP_Synthase_RNA 28
+#define pSREBP 29
+#define HMG_CoA_Synthase_gene 30
+#define Cyp51_gene 31
+#define Cholesterol 32
+#define HMG_CoA_Synthase 33
---
+++ just_new_state/cpu/grn_34_openmp_div_thr.cpp
@@ -0,0 +1,492 @@
+/*
+ * This code was wrote to solve a well defined problems with GRN where the parameters were:
+ * 1 - Number of nodes: - 24
+ * 2 - The names of the nodes are in defines.h (They were read from the grn expressions file)
+ * 3 - The code will find the next state for each initial state between 0x000000000 and 0x3ffffffff
+ * 4 - It will use multi-core system with the available number of threads of the running system
+ */
+
+#include <iostream>
+#include <bits/stdc++.h>
+#include "defines.h"
+
+#define NUM_NOS 34
+
+using namespace std;
+
+void grnPass(){
+    int aux[NUM_NOS];
+    int grn[NUM_NOS];
+    grn[Squaline] = ( grn[Farnesyl_pyrophosphate] );
+    grn[Lanosterol] = ( grn[Squaline] );
+    grn[Acetyl_CoA] = ( grn[Acetyl_CoA] );
+    grn[HMG_CoA_Reductase_RNA] = ( grn[HMG_CoA_Reductase_gene] );
+    grn[Cyp51_RNA] = ( grn[Cyp51_gene] );
+    grn[Septdehydrocholesterol] = ( grn[Lanosterol] );
+    grn[Insig_SREBP_SCAP] =  not ( ( grn[Cholesterol] ) );
+    grn[FPP_Synthase_gene] = ( grn[mSREBP] ) ;
+    grn[HMG_CoA_Reductase_gene] = ( grn[mSREBP] ) ;
+    grn[Acetyl_CoA_acetyltransferase_RNA] = ( grn[Acetyl_CoA_acetyltransferase_gene] ) ;
+    grn[Statins] = ( grn[Statins] ) ;
+    grn[Acetyl_CoA_acetyltransferase_gene] = ( grn[mSREBP] ) ;
+    grn[Desmosterol] = ( grn[Lanosterol] ) ;
+    grn[Mevalonic_Acid] = ( grn[HMG_CoA] and ( ( ( grn[HMG_CoA_Reductase]) ) )    ) ;
+    grn[Acetoacetyl_CoA] = ( grn[Acetyl_CoA] and ( ( ( grn[Acetyl_CoA_acetyltransferase] ) ) )    ) ;
+    grn[Mevalonyl_pyrophosphate] = ( grn[Mevalonic_Acid] );
+    grn[HMG_CoA] = ( grn[Acetoacetyl_CoA] and ( ( ( grn[Acetyl_CoA]  and grn[HMG_CoA_Synthase] ) ) )    ) ;
+    grn[Dimethyl_allyl_pyrophosphate] = ( grn[Isopentenyl_pyrophosphate] ) ;
+    grn[HMG_CoA_Reductase]= ( ( grn[HMG_CoA_Reductase_RNA]  ) and not ( grn[Statins]  ) ) ;
+    grn[FPP_Synthase] = ( grn[FPP_Synthase_RNA] ) ;
+    grn[HMG_CoA_Synthase_RNA] = ( grn[HMG_CoA_Synthase_gene] ) ;
+    grn[SREBP_SCAP] = ( ( grn[Insig_SREBP_SCAP]  ) and not ( grn[Statins]  ) ) ;
+    grn[Geranyl_pyrophosphate] = ( grn[Dimethyl_allyl_pyrophosphate] )  or ( grn[Isopentenyl_pyrophosphate] ) ;
+    grn[Cyp51] = ( grn[Cyp51_RNA] ) ;
+    grn[Isopentenyl_pyrophosphate] = ( grn[Mevalonyl_pyrophosphate] ) ;
+    grn[Acetyl_CoA_acetyltransferase] = ( grn[Acetyl_CoA_acetyltransferase_RNA] ) ;
+    grn[Farnesyl_pyrophosphate] = ( ( grn[Geranyl_pyrophosphate]  ) and not ( grn[FPP_Synthase]  ) ) ;
+    grn[mSREBP] = ( grn[pSREBP] );
+    grn[FPP_Synthase_RNA] = ( grn[FPP_Synthase_gene] ) ;
+    grn[pSREBP] = ( grn[SREBP_SCAP] ) ;
+    grn[HMG_CoA_Synthase_gene] = ( grn[mSREBP] ) ;
+    grn[Cyp51_gene] = ( grn[mSREBP] ) ;
+    grn[Cholesterol] = ( grn[Desmosterol] )  or ( grn[Septdehydrocholesterol] ) ;
+    grn[HMG_CoA_Synthase] = ( grn[HMG_CoA_Synthase_RNA] ) ;
+
+}
+
+int main(){
+
+    // default constructor initializes with all bits 0
+    bitset<NUM_NOS> bset1;
+
+    // bset2 is initialized with bits of 20
+    bitset<NUM_NOS> bset2(20);
+
+    // bset3 is initialized with bits of specified binary string
+    bitset<NUM_NOS> bset3(string("1100"));
+
+    // cout prints exact bits representation of bitset
+    cout << bset1 << endl; // 00000000000000000000000000000000
+    cout << bset2 << endl; // 00000000000000000000000000010100
+    cout << bset3 << endl; // 00000000000000000000000000001100
+    cout << endl;
+
+    // declaring set8 with capacity of 8 bits
+
+    bitset<8> set8; // 00000000
+
+    // setting first bit (or 6th index)
+    set8[1] = 1; // 00000010
+    set8[4] = set8[1]; // 00010010
+    cout << set8 << endl;
+
+    // count function returns number of set bits in bitset
+    int numberof1 = set8.count();
+
+    // size function returns total number of bits in bitset
+    // so there difference will give us number of unset(0)
+    // bits in bitset
+    int numberof0 = set8.size() - numberof1;
+
+    cout << set8 << " has " << numberof1 << " ones and "
+         << numberof0 << " zeros\n";
+
+    // test function return 1 if bit is set else returns 0
+    cout << "bool representation of " << set8 << " : ";
+    for (int i = 0; i < set8.size(); i++)
+        cout << set8.test(i) << " ";
+
+    cout << endl;
+
+    // any function returns true, if atleast 1 bit
+    // is set
+    if (!set8.any())
+        cout << "set8 has no bit set.\n";
+
+    if (!bset1.any())
+        cout << "bset1 has no bit set.\n";
+
+    // none function returns true, if none of the bit
+    // is set
+    if (!bset1.none())
+        cout << "bset1 has some bit set\n";
+
+    // bset.set() sets all bits
+    cout << set8.set() << endl;
+
+    // bset.set(pos, b) makes bset[pos] = b
+    cout << set8.set(4, 0) << endl;
+
+    // bset.set(pos) makes bset[pos] = 1  i.e. default
+    // is 1
+    cout << set8.set(4) << endl;
+
+    // reset function makes all bits 0
+    cout << set8.reset(2) << endl;
+    cout << set8.reset() << endl;
+
+    // flip function flips all bits i.e.  1 <-> 0
+    // and  0 <-> 1
+    cout << set8.flip(2) << endl;
+    cout << set8.flip() << endl;
+
+    // Converting decimal number to binary by using bitset
+    int num = 100;
+    cout << "\nDecimal number: " << num
+         << "  Binary equivalent: " << bitset<8>(num);
+
+    return 0;
+}
+
+//void pass(){
+
+//}
+
+
+/*
+int main(int argc, char **argv) {
+    InputParser input(argc, argv);
+
+    string filein = input.getCmdOption("-i");
+    string fileout = input.getCmdOption("-o");
+
+    if(!filein.size() || !fileout.size()) {
+        cout << "Usage: ./<executable> -i <input filename> -o <utput filename csv>" << endl;
+        exit(255);
+    }
+
+    uint32_t **period_vet;
+    uint32_t **transient_vet;
+    uint64_t *init_state;
+    uint64_t *end_state;
+    uint32_t lines;
+
+    bool **s0;
+    bool **s1;
+
+    uint64_t **atractor_vet;
+
+    readInputFile(&period_vet, &transient_vet, &init_state, &end_state, &s0, &s1, &atractor_vet, &lines, filein);
+
+    if(period_vet == nullptr || transient_vet == nullptr || init_state == nullptr || end_state == nullptr) {
+        cout << "Error: malloc failed!" << endl;
+        exit(255);
+    }
+
+    high_resolution_clock::time_point s;
+    duration<double> diff{};
+    s = high_resolution_clock::now();
+
+    runGNR(init_state, end_state, period_vet, transient_vet, s0, s1, atractor_vet, lines);
+
+    diff = high_resolution_clock::now() - s;
+
+    // cout << "Number of state: " << NUM_STATES << endl;
+    cout << "Execution Time: " << diff.count() * 1000 << "ms" << endl;
+    // cout << "State per second: " << (NUM_STATES/diff.count()) << endl;
+
+    writeOutputFile(period_vet, transient_vet, init_state, end_state, atractor_vet, lines, fileout);
+
+    // free(period_vet);
+    // free(transient_vet);
+
+    return 0;
+}
+
+
+/*
+#include <iostream>
+#include <math.h>
+#include <omp.h>
+#include <chrono>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+
+#define NUM_NOS 34
+
+using namespace std;
+using namespace std::chrono;
+
+vector<string> splitString(string s, string delimiter);
+void readInputFile(uint32_t ***period, uint32_t ***transient, uint64_t **init_state, uint64_t **end_state, bool ***s0, bool ***s1, uint64_t ***atractor, uint32_t *lines, string filename);
+void pass(bool *aux);
+bool equals(bool *vet1, bool *vet2);
+string vet_to_hex(bool *vet1);
+void initialState(uint64_t valor, bool *vet1, bool *vet2);
+string boolArraytoString(bool *vet);
+uint64_t boolArraytoInt(bool *vet);
+void runGNR(uint64_t *init_state, uint64_t *end_state, uint32_t **period_vet, uint32_t **transient_vet, bool **s0, bool **s1, uint64_t **atractor_vet, uint32_t lines);
+void writeOutputFile(uint32_t **period, uint32_t **transient, uint64_t *init_state, uint64_t *end_state, uint64_t **atractor, uint32_t lines, string filename);
+
+class InputParser {
+private:
+    vector<string> tokens;
+
+public:
+    InputParser(int &argc, char **argv) {
+        for(int i=1; i<argc; ++i) {
+            this->tokens.push_back(string(argv[i]));
+        }
+    }
+
+    bool cmdOptionExists(const string &option) const{
+        return find(this->tokens.begin(), this->tokens.end(), option)
+                != this->tokens.end();
+    }
+
+    const string& getCmdOption(const string &option) const{
+        vector<string>::const_iterator itr;
+        itr =  find(this->tokens.begin(), this->tokens.end(), option);
+        if (itr != this->tokens.end() && ++itr != this->tokens.end() && (*itr).front() != '-'){
+            return *itr;
+        }
+        static const string empty_string("");
+        return empty_string;
+    }
+};
+
+int main(int argc, char **argv) {
+    InputParser input(argc, argv);
+
+    string filein = input.getCmdOption("-i");
+    string fileout = input.getCmdOption("-o");
+
+    if(!filein.size() || !fileout.size()) {
+        cout << "Usage: ./<executable> -i <input filename> -o <utput filename csv>" << endl;
+        exit(255);
+    }
+
+    uint32_t **period_vet = nullptr;
+    uint32_t **transient_vet = nullptr;
+    uint64_t *init_state = nullptr;
+    uint64_t *end_state = nullptr;
+    uint32_t lines;
+
+    bool **s0 = nullptr;
+    bool **s1 = nullptr;
+
+    uint64_t **atractor_vet;
+
+    readInputFile(&period_vet, &transient_vet, &init_state, &end_state, &s0, &s1, &atractor_vet, &lines, filein);
+
+    if(period_vet == nullptr || transient_vet == nullptr || init_state == nullptr || end_state == nullptr) {
+        cout << "Error: malloc failed!" << endl;
+        exit(255);
+    }
+
+    high_resolution_clock::time_point s;
+    duration<double> diff{};
+    s = high_resolution_clock::now();
+
+    runGNR(init_state, end_state, period_vet, transient_vet, s0, s1, atractor_vet, lines);
+
+    diff = high_resolution_clock::now() - s;
+
+    // cout << "Number of state: " << NUM_STATES << endl;
+    cout << "Execution Time: " << diff.count() * 1000 << "ms" << endl;
+    // cout << "State per second: " << (NUM_STATES/diff.count()) << endl;
+
+    writeOutputFile(period_vet, transient_vet, init_state, end_state, atractor_vet, lines, fileout);
+
+    // free(period_vet);
+    // free(transient_vet);
+
+    return 0;
+}
+
+void writeOutputFile(uint32_t **period, uint32_t **transient, uint64_t *init_state, uint64_t *end_state, uint64_t **atractor, uint32_t lines, string filename) {
+    ofstream file(filename);
+    for(uint32_t l=0; l<lines; ++l) {
+        for(uint64_t s=init_state[l], i = 0; s <= end_state[l]; ++s, ++i) {
+            file << to_string(l) << ", " << to_string(period[l][i]) << ", " << to_string(transient[l][i]) << ", " << std::hex << (atractor[l][i]) << endl;
+        }
+    }
+    file.close();
+}
+
+vector<string> splitString(string s, string delimiter) {
+    size_t pos = 0;
+    vector<string> ans;
+    while((pos = s.find(delimiter)) != string::npos) {
+        ans.push_back(s.substr(0, pos));
+        s.erase(0, pos + delimiter.length());
+    }
+    ans.push_back(s);
+    return ans;
+}
+
+void readInputFile(uint32_t ***period, uint32_t ***transient, uint64_t **init_state, uint64_t **end_state, bool ***s0, bool ***s1, uint64_t ***atractor, uint32_t *lines, string filename) {
+    ifstream file(filename);
+    uint32_t cnt_lines = 0;
+    string line;
+    uint32_t size;
+    vector<vector<string>> all_items;
+    while(getline(file, line)) {
+        auto items = splitString(line, ",");
+        if(items.size() == 4) {
+            cnt_lines++;
+            all_items.push_back(items);
+        }
+    }
+    file.close();
+
+    (*lines) = cnt_lines;
+    (*period) = (uint32_t **) malloc(cnt_lines * sizeof (uint32_t *));
+    (*transient) = (uint32_t **) malloc(cnt_lines * sizeof(uint32_t *));
+    (*init_state) = (uint64_t *) malloc(cnt_lines * sizeof(uint64_t));
+    (*end_state) = (uint64_t *) malloc(cnt_lines * sizeof(uint64_t));
+    (*s0) = (bool **) malloc(cnt_lines * sizeof(bool *));
+    (*s1) = (bool **) malloc(cnt_lines * sizeof(bool *));
+    (*atractor) = (uint64_t **) malloc(cnt_lines * sizeof (uint64_t *));
+
+    for(int i=0; i<cnt_lines; ++i) {
+        size = stoul(all_items[i][3], NULL, 10);
+        (*period)[i] = (uint32_t *) malloc(size * sizeof(uint32_t));
+        (*transient)[i] = (uint32_t *) malloc(size * sizeof(uint32_t));
+        (*init_state)[i] = stoull(all_items[i][1], NULL, 16);
+        (*end_state)[i] = stoull(all_items[i][2], NULL, 16);
+        (*s1)[i] = (bool *) malloc(NUM_NOS * sizeof(bool));
+        (*s0)[i] = (bool *) malloc(NUM_NOS * sizeof(bool));
+        (*atractor)[i] = (uint64_t *) malloc(size * sizeof(uint64_t));
+    }
+}
+
+void runGNR(uint64_t *init_state, uint64_t *end_state, uint32_t **period_vet, uint32_t **transient_vet, bool **s0, bool **s1, uint64_t **atractor_vet, uint32_t lines) {
+    #pragma omp parallel for schedule(static)
+    for(uint32_t l=0; l<lines; ++l) {
+        for (uint64_t s = init_state[l], i = 0; s <= end_state[l]; s++, i++) {
+            initialState(s, s0[l], s1[l]);
+            transient_vet[l][i] = 0;
+            period_vet[l][i] = 0;
+            do {
+                pass(s0[l]);
+                pass(s1[l]);
+                pass(s1[l]);
+                // transient++;
+                transient_vet[l][i]++;
+            } while (!equals(s0[l], s1[l]));
+            do {
+                pass(s1[l]);
+                // period++;
+                period_vet[l][i]++;
+            } while (!equals(s0[l], s1[l]));
+
+            period_vet[l][i]--;
+            atractor_vet[l][i] = boolArraytoInt(s0[l]);
+            // printf("%lld: P: %u T: %u\n", i, period, transient);
+            /* if(output.is_open()) {
+                string str = "";
+                str += to_string()
+                str += to_string(i);
+                str += ",";
+                str += to_string(period);
+                str += ",";
+                str += to_string(transient);
+                str += "\n";
+                cout << str << endl;
+                output.write(str.c_str(), str.size());
+            } *//*
+        }
+    }
+}
+
+void initialState(uint64_t valor, bool *vet1, bool *vet2) {
+    for (int i = 0; i < NUM_NOS; i++) {
+        vet1[i] = (valor & 1) != 0;
+        vet2[i] = vet1[i];
+        valor >>= 1;
+    }
+}
+
+bool equals(bool *vet1, bool *vet2) {
+    for (int i = 0; i < NUM_NOS; i++) {
+        if (vet1[i] != vet2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+string vet_to_hex(bool *vet) {
+    string hex = "";
+    for(int i=0; i<NUM_NOS; i += 4) {
+        int v=0;
+        for(int j=i+3; j>=i; --j) {
+            if(j < NUM_NOS) {
+                v = (v << 1) | (vet[j] ? 1 : 0);
+            }
+        }
+        if(v >= 10) hex += ('a' + v - 10);
+        else hex += to_string(v);
+    }
+    reverse(hex.begin(), hex.end());
+
+    return hex;
+}
+
+string boolArraytoString(bool *vet) {
+    string out = "";
+    for (int i = NUM_NOS - 1; i >= 0; i--) {
+        if (vet[i]) {
+            out += "1";
+        } else {
+            out += "0";
+        }
+    }
+    return out;
+}
+
+uint64_t boolArraytoInt(bool *vet) {
+    uint64_t out = 0;
+    // std::cout <<"boolArraytoInt:"<< std::endl;
+    for (int i = NUM_NOS - 1; i >= 0; i--) {
+        //   std::cout << vet[i] << " ";
+        out = (out << 1ll) | vet[i];
+    }
+    //  std::cout << " = " << out << std::endl;
+    return out;
+}
+
+void pass(bool *aux) {
+    bool vet[NUM_NOS];
+    for (int i=0; i<NUM_NOS; i++){
+        vet[i] = aux[i];
+    }
+    aux[31]  =   (  vet[11]  )   ;
+    aux[25]  =   (  vet[31]  )   ;
+    aux[1]  =   (  vet[1]  )   ;
+    aux[19]  =   (  vet[18]  )   ;
+    aux[8]  =   (  vet[7]  )   ;
+    aux[30]  =   (  vet[25]  )   ;
+    aux[23]  =   !  (   (  vet[5]  )   )  ;
+    aux[13]  =   (  vet[28]  )   ;
+    aux[18]  =   (  vet[28]  )   ;
+    aux[4]  =   (  vet[3]  )   ;
+    aux[33]  =   (  vet[33]  )   ;
+    aux[3]  =   (  vet[28]  )   ;
+    aux[9]  =   (  vet[25]  )   ;
+    aux[26]  =   (  vet[16] &&  (   (   (  vet[17]  )   )   )      )   ;
+    aux[0]  =   (  vet[1] &&  (   (   (  vet[2]  )   )   )      )   ;
+    aux[27]  =   (  vet[26]  )   ;
+    aux[16]  =   (  vet[0] &&  (   (   (  vet[1]  && vet[20]  )   )   )      )   ;
+    aux[10]  =   (  vet[24]  )   ;
+    aux[17]  =   (   (  vet[19]   )  && !  (  vet[33]   )   )   ;
+    aux[12]  =   (  vet[14]  )   ;
+    aux[22]  =   (  vet[21]  )   ;
+    aux[32]  =   (   (  vet[23]   )  && !  (  vet[33]   )   )   ;
+    aux[15]  =   (  vet[10]  )   ||  (  vet[24]  )   ;
+    aux[6]  =   (  vet[8]  )   ;
+    aux[24]  =   (  vet[27]  )   ;
+    aux[2]  =   (  vet[4]  )   ;
+    aux[11]  =   (   (  vet[15]   )  && !  (  vet[12]   )   )   ;
+    aux[28]  =   (  vet[29]  )   ;
+    aux[14]  =   (  vet[13]  )   ;
+    aux[29]  =   (  vet[32]  )   ;
+    aux[21]  =   (  vet[28]  )   ;
+    aux[7]  =   (  vet[28]  )   ;
+    aux[5]  =   (  vet[9]  )   ||  (  vet[30]  )   ;
+    aux[20]  =   (  vet[22]  )   ;
+
+}
+*/
